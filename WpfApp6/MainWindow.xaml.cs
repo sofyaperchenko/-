778d5abb4c7bc714df320f63ac1b449c179195c6
Honor <Honor@DESKTOP-I9SS3US>
2025-01-14 11:12:38 +0300

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    
+    public partial class MainWindow : Window
+    {
+        private Random random;
+
+        private int balance; 
+        private const int BetAmount = 10; 
+        public MainWindow()
+             
+        {
+            InitializeComponent();
+            random = new Random();
+            balance = 100; 
+            UpdateBalanceDisplay();
+        }
+        private void RollButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (balance < BetAmount)
+            {
+                ResultTextBlock.Text = "Недостаточно средств для ставки!";
+                return;
+            }
+            int die1 = random.Next(1, 7); 
+            int die2 = random.Next(1, 7); 
+            int die3 = random.Next(1, 7);
+
+            DieText1.Text = die1.ToString();
+            DieText2.Text = die2.ToString();
+            DieText3.Text = die3.ToString();
+            
+            int total = die1 + die2 + die3; 
+
+            ResultTextBlock.Text = $"Сумма: {total}.";
+
+            if (total >= 10) 
+            {
+                balance += BetAmount; 
+                ResultTextBlock.Text += $" Вы выиграли {BetAmount} единиц!";
+            }
+            else 
+            {
+                balance -= BetAmount; 
+                ResultTextBlock.Text += $" Вы проиграли {BetAmount} единиц.";
+            }
+
+            UpdateBalanceDisplay(); 
+        }
+
+        private void UpdateBalanceDisplay()
+        {
+            BalanceTextBlock.Text = $"Баланс: {balance} единиц";
+        }
+    }
+}
+
